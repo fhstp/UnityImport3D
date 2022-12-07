8281a9745b5f3f76ec6c0e5e10e454f8f08297e2
ComradeVanti <kingvanti650@gmail.com>
2022-12-07 13:45:53 +0100

    refactor: split tests

--- Tests/Model/EmbeddedTextureToModelTests.cs
+++ Tests/Model/EmbeddedTextureToModelTests.cs
@@ -10,7 +10,7 @@ namespace At.Ac.FhStp.Import3D
 {
 
     [RequiresPlayMode(false)]
-    public class AssimpToModelTests
+    public class EmbeddedTextureToModelTests
     {
 
         private static AssimpTexel[] MakeBlackTexels(int width, int height) =>
@@ -21,54 +21,6 @@ namespace At.Ac.FhStp.Import3D
             int width, int height, string name) =>
             new AssimpEmbeddedTexture(width, height,
                                       MakeBlackTexels(width, height), name);
-
-        [Test]
-        public async Task Pixel_Color_R_Is_Assimp_Texel_R_Normalized_To_01()
-        {
-            const byte r = 100;
-            const float rNormalized = r / 255f;
-            var texel = new AssimpTexel(0, 0, r, 0);
-
-            var pixel = await AssimpToModel.Texel(texel);
-
-            Assert.AreEqual(rNormalized, pixel.r);
-        }
-
-        [Test]
-        public async Task Pixel_Color_G_Is_Assimp_Texel_G_Normalized_To_01()
-        {
-            const byte g = 100;
-            const float gNormalized = g / 255f;
-            var texel = new AssimpTexel(0, g, 0, 0);
-
-            var pixel = await AssimpToModel.Texel(texel);
-
-            Assert.AreEqual(gNormalized, pixel.g);
-        }
-
-        [Test]
-        public async Task Pixel_Color_B_Is_Assimp_Texel_B_Normalized_To_01()
-        {
-            const byte b = 100;
-            const float bNormalized = b / 255f;
-            var texel = new AssimpTexel(b, 0, 0, 0);
-
-            var pixel = await AssimpToModel.Texel(texel);
-
-            Assert.AreEqual(bNormalized, pixel.b);
-        }
-
-        [Test]
-        public async Task Pixel_Color_A_Is_Assimp_Texel_A_Normalized_To_01()
-        {
-            const byte a = 100;
-            const float aNormalized = a / 255f;
-            var texel = new AssimpTexel(0, 0, 0, a);
-
-            var pixel = await AssimpToModel.Texel(texel);
-
-            Assert.AreEqual(aNormalized, pixel.a);
-        }
 
         [Test]
         public async Task CompressedTexture_Model_Name_Is_Assimp_Filename_Without_Extension()
