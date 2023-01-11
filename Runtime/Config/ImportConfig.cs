209ca17d944269f9bb4c3e169c268aa40ec9058a
Ramon Brullo <kingvanti650@gmail.com>
2023-01-11 11:39:35 +0100

    feat: functions for building configs

--- Runtime/Config/ImportConfig.cs
+++ Runtime/Config/ImportConfig.cs
@@ -27,12 +27,37 @@ namespace At.Ac.FhStp.Import3D
         ///     Triangulate is enforced for every import by default
         /// </summary>
         /// <seealso href="https://documentation.help/assimp/postprocess_8h.html#a64795260b95f5a4b3f3dc1be4f52e410">Assimp documentation</seealso>
-        public PostProcessSteps ExtraAssimpPostProcessSteps { get; init; }
+        public PostProcessSteps ExtraAssimpPostProcessSteps
+        {
+            get;
+            private init;
+        }
 
         /// <summary>
         ///     By default the name of the scene is taken to be name name of the imported file.
         ///     If this property is set so Some string it will override this behaviour.
         /// </summary>
-        public IOpt<string> SceneNameOverride { get; init; }
+        public IOpt<string> SceneNameOverride { get; private init; }
+
+
+        /// <summary>
+        ///     Replaces the extra post-processing steps in this config
+        /// </summary>
+        /// <param name="steps">The replacement steps</param>
+        /// <returns>Updated config</returns>
+        public ImportConfig WithExtraAssimpPostProcessSteps(
+            PostProcessSteps steps) =>
+            this with {ExtraAssimpPostProcessSteps = steps};
+
+        /// <summary>
+        ///     By default the scene-name is the name of the file the scene was
+        ///     loaded from. Use this function to specify a custom name
+        /// </summary>
+        /// <param name="name">The name</param>
+        /// <returns>The updated config</returns>
+        public ImportConfig WithSceneName(string name) => this with
+        {
+            SceneNameOverride = Opt.Some(name)
+        };
     }
 }
