fb78799fe17f6016129c2f8bf0c305e469fbfdec
Ramon Brullo <kingvanti650@gmail.com>
2023-04-20 17:13:14 +0200

    feat: import ambient color

--- Runtime/Materials/ModelConversion.cs
+++ Runtime/Materials/ModelConversion.cs
@@ -4,19 +4,23 @@ namespace At.Ac.FhStp.Import3D.Materials
 {
     internal static class ModelConversion
     {
-        
         private static Color ConvertColor(Assimp.Color4D color)
         {
             return new Color(color.R, color.G, color.B, color.A);
         }
-        
+
         internal static MaterialModel ConvertToModel(Assimp.Material assimpMaterial)
         {
-            var color = ConvertColor(assimpMaterial.ColorDiffuse);
+            var diffuse = assimpMaterial.HasColorDiffuse
+                ? ConvertColor(assimpMaterial.ColorDiffuse)
+                : Color.black;
+            var ambient = assimpMaterial.HasColorAmbient
+                ? ConvertColor(assimpMaterial.ColorAmbient)
+                : Color.black;
+            var color = diffuse + ambient;
             color.a *= assimpMaterial.Opacity;
-            
+
             return new MaterialModel(assimpMaterial.Name, color);
         }
-        
     }
 }
