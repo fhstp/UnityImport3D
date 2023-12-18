8631b6ae44d8493910eb10052edf43eb73fb95cf
ComradeVanti <kingvanti650@gmail.com>
2023-12-18 12:41:04 +0100

    feat(import-tester): Import models from path

---
+++ Samples~/ImportTester/Code/Importer.cs
@@ -0,0 +1,106 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace At.Ac.FhStp.Import3D.ImportTester
+{
+    public class Importer : MonoBehaviour
+    {
+        [SerializeField] private Transform modelParent = null!;
+
+        private CancellationTokenSource? importCancelSource;
+
+        private void ClearCurrentModels()
+        {
+            for (var i = 0; i < modelParent.childCount; i++)
+            {
+                var child = modelParent.GetChild(i);
+                Destroy(child.gameObject);
+            }
+        }
+
+        private async Task ImportModels(CancellationToken ct)
+        {
+            var modelDirectory = Path.Combine(
+                System.Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                "3D Objects/ImportTester");
+
+            if (!Directory.Exists(modelDirectory))
+            {
+                Debug.LogError($"Directory \"{modelDirectory}\" does not exist!");
+                return;
+            }
+
+            var config = ImportConfig.Default with
+            {
+                ActivateRoot = false,
+            };
+
+            async Task<GameObject?> ImportSingle(string path)
+            {
+                try
+                {
+                    var model = await Import.SingleAsync(path, config);
+
+                    if (ct.IsCancellationRequested)
+                    {
+                        DestroyImmediate(model);
+                        return null;
+                    }
+
+                    return model;
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e);
+                }
+
+                return null;
+            }
+
+            var filePaths = Directory.GetFiles(modelDirectory)
+                                     .Where(Import.SupportsFileExtension);
+            var importTasks = filePaths.Select(ImportSingle);
+            var models = await Task.WhenAll(importTasks);
+
+            ClearCurrentModels();
+
+            foreach (var model in models)
+            {
+                if (model == null) continue;
+                model.transform.SetParent(modelParent, true);
+            }
+        }
+
+        private void CancelImport()
+        {
+            importCancelSource?.Cancel();
+            importCancelSource = null;
+        }
+
+        public void StartImportingModels()
+        {
+            CancelImport();
+
+            importCancelSource = new CancellationTokenSource();
+            _ = ImportModels(importCancelSource.Token);
+        }
+
+        private void OnDisable()
+        {
+            CancelImport();
+        }
+
+        private void Start()
+        {
+            StartImportingModels();
+        }
+    }
+}
--- Samples~/ImportTester/README.md
+++ Samples~/ImportTester/README.md
@@ -2,3 +2,8 @@
 
 In this sample you can test the importing functionality
 of the package as well as get to know the API.
+
+## Usage
+
+On start-up the sample will load all models inside `[User-folder]/3D Models/ImportTester`.
+
